48ea872d388afa3e0972c438cc8a9afabbc101e5
David Pollard <pollardld@gmail.com>
2019-05-15 13:19:47 -0700

    sample set and v1 template ready to test

---
+++ scripts/generate_comet_input_file.py
@@ -0,0 +1,20 @@
+# import system modules
+import os, sys
+
+# import functions for extracting data from excel
+from openpyxl import load_workbook
+
+# check if argument for workbook has been given
+if len( sys.argv ) < 1:
+    print "\n"
+    print "python script generate_comet_input_file.py <spreadsheet location>"
+    print "\n"
+    print "Command-line arguments are as follows:"
+    print "  <spreadsheet locatiion> system location of spreadsheet with input data"
+    print "    eg /usr/local/name/comet/data.xml"
+    print ""
+    exit()
+
+wb_dir = sys.argv[1]
+wb = load_workbook(filename = wb_dir)
+print(wb.sheetnames)
