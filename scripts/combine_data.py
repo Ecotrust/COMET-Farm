4837536f58055256fe383825d58f63d315f7b19e
David Pollard <pollardld@gmail.com>
2019-05-17 15:55:27 -0700

    Combine data with sample data ready

--- scripts/combine_data.py
+++ scripts/combine_data.py
@@ -39,10 +39,16 @@ with open(gis_dir) as csv_file:
         field_sheet = wb.copy_worksheet(scenario_sheet)
         for rowNum in range(2, field_sheet.max_row):
             rowName = field_sheet.cell(row=rowNum, column=1).value
+            if rowName == 'id':
+                field_sheet.cell(row=rowNum, column=2).value = row['field_ID']
             if rowName == 'GEOM':
-                field_sheet.cell(row=rowNum, column=2).value = "Polygon(" + row['geom'] + ")"
+                field_sheet.cell(row=rowNum, column=2).value = 'Polygon(" + row[\'geom\'] + ")'
             if rowName == 'AREA':
                 field_sheet.cell(row=rowNum, column=2).value = row['acres']
+            if rowName == 'SRID':
+                field_sheet.cell(row=rowNum, column=2).value = '4326'
+            if rowName == 'crop_scenario_name':
+                field_sheet.cell(row=rowNum, column=2).value = 'Todo_polygon_id' + row['field_ID'] + 'Todo_scenario_id'
             if rowName == 'Ccop_name':
                 field_sheet.cell(row=rowNum, column=2).value = row['comet_crop']
             if rowName == 'pre_80':
@@ -50,5 +56,15 @@ with open(gis_dir) as csv_file:
                 pre_80_gis_val = row['pre-1980']
                 pre_1980_val = pre_1980_sheet.cell(row=int(pre_80_gis_val), column=1).value
                 field_sheet.cell(row=rowNum, column=2).value = pre_1980_val
+            if rowName == 'yr80_2000':
+                year80_sheet = wb.get_sheet_by_name('yr80')
+                year80_sheet_val = row['Year1980-2000']
+                yr_80_val = year80_sheet.cell(row=int(year80_sheet_val), column=1).value
+                field_sheet.cell(row=rowNum, column=2).value = yr_80_val
+            if rowName == 'till80_200':
+                till_80_sheet = wb.get_sheet_by_name('tillage')
+                till_80_sheet_val = row['Year1980-2000_Tillage']
+                till_80_val = till_80_sheet.cell(row=int(till_80_sheet_val), column=1).value
+                field_sheet.cell(row=rowNum, column=2).value = till_80_val
 
 wb.save('combined_data.xlsx')
